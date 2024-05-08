3fe6cd994be79220e13e057d32ec81bc34449900
JhordanRicaldi <77322350@continental.edu.pe>
2024-05-07 20:58:45 -0500

    commit inicial

--- FRONTEND/odontograma/src/app/app.component.ts
+++ FRONTEND/odontograma/src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'registrarPaciente';
-
-  constructor() {}
-  ngOnInit() {}
-
-  onSunmit() {}
+export class AppComponent {
+  title = 'odontograma';
 }
