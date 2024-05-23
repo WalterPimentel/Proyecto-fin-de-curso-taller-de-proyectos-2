4136ca91c7bfd7a2bcbfa42787820ab06246cced
WalterPimentel <70056787@continental.edu.pe>
2024-05-23 13:28:45 -0500

    Logro: Pruebas Unitarias básicas

--- FRONTEND/odontograma/src/app/app.component.spec.ts
+++ FRONTEND/odontograma/src/app/app.component.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { OdontogramaComponent } from './components/odontograma/odontograma.component';
+import { OdontogramaAnatomicoUIComponent } from './components/ui/odontograma-anatomico/anatomico.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [AppComponent, OdontogramaComponent, OdontogramaAnatomicoUIComponent],
     }).compileComponents();
   });
 
@@ -30,6 +29,15 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, odontograma');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Odontograma'
+    );
+  });
+
+  it('should change title when call changeTitle method', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.changeTitle('nuevo titulo');
+    expect(app.title).toEqual('nuevo titulo');
   });
 });
