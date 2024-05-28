7d0c2ed5f914e48992406e71e557b781141a9704
WalterPimentel <70056787@continental.edu.pe>
2024-05-28 12:47:42 -0500

    Proceso: maqueta de diente geométrico

--- FRONTEND/odontograma/src/app/components/odontograma.module.ts
+++ FRONTEND/odontograma/src/app/components/odontograma.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { OdontogramaComponent } from './odontograma/odontograma.component';
 import { OdontogramaAnatomicoUIComponent } from './ui/odontograma-anatomico/anatomico.component';
-import { SpinnerComponent } from './ui/spinner/spinner.component'
+import { OdontogramaGeometricoUIComponent } from './ui/odontograma-geometrico/geometrico.component';
+import { SpinnerComponent } from './ui/spinner/spinner.component';
 
 @NgModule({
-  declarations: [OdontogramaAnatomicoUIComponent, OdontogramaComponent, SpinnerComponent],
+  declarations: [
+    OdontogramaAnatomicoUIComponent,
+    OdontogramaGeometricoUIComponent,
+    OdontogramaComponent,
+    SpinnerComponent,
+  ],
   imports: [CommonModule, FormsModule],
   exports: [OdontogramaComponent],
 })
