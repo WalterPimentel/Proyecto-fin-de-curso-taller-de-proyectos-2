6738b11cb8b1ae124272653b0ebfd5878e2bd34b
WalterPimentel <70056787@continental.edu.pe>
2024-05-28 23:52:30 -0500

    Logrado: Maqueta Odontograma Geométrico

--- FRONTEND/odontograma/src/app/components/ui/odontograma-geometrico/geometrico.component.ts
+++ FRONTEND/odontograma/src/app/components/ui/odontograma-geometrico/geometrico.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ui-odontograma-geometrico',
@@ -6,5 +6,49 @@ import { Component, ChangeDetectorRef } from '@angular/core';
   styleUrls: ['./geometrico.component.css'],
 })
 export class OdontogramaGeometricoUIComponent {
-    
+
+  @Input() edadCategoria!: string;
+
+  dientes = Array(8)
+    .fill(0)
+    .map((x, i) => i + 11)
+    .reverse()
+    .concat(
+      Array(8)
+        .fill(0)
+        .map((x, i) => i + 21)
+    )
+    .concat(
+      Array(8)
+        .fill(0)
+        .map((x, i) => i + 41)
+        .reverse()
+    )
+    .concat(
+      Array(8)
+        .fill(0)
+        .map((x, i) => i + 31)
+    )
+    .concat(
+      Array(5)
+        .fill(0)
+        .map((x, i) => i + 51)
+        .reverse()
+    )
+    .concat(
+      Array(5)
+        .fill(0)
+        .map((x, i) => i + 61)
+    )
+    .concat(
+      Array(5)
+        .fill(0)
+        .map((x, i) => i + 81)
+        .reverse()
+    )
+    .concat(
+      Array(5)
+        .fill(0)
+        .map((x, i) => i + 71)
+    );
 }
