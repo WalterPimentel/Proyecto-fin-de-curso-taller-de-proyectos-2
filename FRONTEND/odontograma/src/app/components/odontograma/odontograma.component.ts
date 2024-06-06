0725d386f5e99e0ecafdf48767f9002b505ddef1
WalterPimentel <70056787@continental.edu.pe>
2024-06-05 21:33:29 -0500

    Logrado: Refactoring y Diseño Validaciones

--- FRONTEND/odontograma/src/app/components/odontograma/odontograma.component.ts
+++ FRONTEND/odontograma/src/app/components/odontograma/odontograma.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 import { HistoriaClinicaService } from '../../services/historiaClinica.service';
@@ -6,14 +6,6 @@ import { OdontogramaService } from '../../services/odontograma.service';
 import { ModalUIComponent } from '../ui/modal/modal.component';
 
 import axios from 'axios';
-
-interface ToothTreatment {
-  tratamiento: string;
-  pieza: string;
-  cara: string;
-  color: string;
-  icono: string;
-}
 
 @Component({
   selector: 'app-odontograma',
@@ -27,94 +19,11 @@ export class OdontogramaComponent {
   edadCategoria: string = '';
   tipoOdontograma: string;
   fechaActual = new Date();
-  selectedRowIndex: number | null = null;
-  datos: ToothTreatment[] = [];
-  modalOpen: boolean = false;
-  selectedTooth: string | null = null;
-  selectedTreatment: string = '';
   odontograma: any;
   form: FormGroup;
   public saveButtonPressed = false;
   formInvalid: boolean = false;
   @ViewChild('modal') modal!: ModalUIComponent;
-
-  tratamientos = [
-    'Apiceptomía',
-    'Carillas',
-    'Cirugía',
-    'Contanto Alimento',
-    'Corona',
-    'Curetaje',
-    'Endodoncia',
-    'Esqueletico',
-    'Estética',
-    'Exploración',
-    'Extrusión',
-    'Furcas',
-    'Girar',
-    'Impacto Alimento',
-    'Impresiones',
-    'Inclinacion',
-    'Limpieza',
-    'Movilidad',
-    'Obturacion',
-    'Ortodoncia',
-    'Perno',
-    'Pilar solo',
-    'Pilar transepitelial',
-    'Placa descarga',
-    'Protesis removible',
-    'Puente',
-    'Quitar',
-    'Radiografia',
-    'Reconstrucción',
-    'Sangrado',
-    'Sellador',
-    'Sensibilidad',
-    'Supurado',
-    'Tornillo',
-    'Tornillo Solo',
-    'Tratamiento',
-  ];
-
-  tratamientoColores: { [key: string]: string } = {
-    Apiceptomía: '#FF5733',
-    Carillas: '#00BBFF',
-    Cirugía: '#A1FF33',
-    'Contanto Alimento': '#FF33A1',
-    Corona: '#AB9FC3',
-    Curetaje: '#25A668',
-    Endodoncia: '#33FF57',
-    Esqueletico: '#BBFF00',
-    Estética: '#00FFBB',
-    Exploración: '#66FF33',
-    Extrusión: '#FFA133',
-    Furcas: '#C3A6A6',
-    Girar: '#E6FF50',
-    'Impacto Alimento': '#33FFA1',
-    Impresiones: '#45Fa33',
-    Inclinacion: '#F9F6BC',
-    Limpieza: '#54EFFF',
-    Movilidad: '#456D3E',
-    Obturacion: '#FFBCF5',
-    Ortodoncia: '#F1DABE',
-    Perno: '#FF7A00',
-    'Pilar solo': '#A35E8F',
-    'Pilar transepitelial': '#2395FF',
-    'Placa descarga': '#8BECE6',
-    'Protesis removible': '#D39BFF',
-    Puente: '#B89BDD',
-    Quitar: '#AC1F00',
-    Radiografia: '#9C7FC0',
-    Reconstrucción: '#0CA3C3',
-    Sangrado: '#FF3366',
-    Sellador: '#A133FF',
-    Sensibilidad: '#E48BEC',
-    Supurado: '#9F0DAC',
-    Tornillo: '#E5DDFE',
-    'Tornillo Solo': '#D4FEFF',
-    Tratamiento: '#008826',
-  };
 
   formatDate(date: Date | string): string {
     let validDate: Date;
@@ -135,71 +44,15 @@ export class OdontogramaComponent {
   }
 
   constructor(
-    private cdr: ChangeDetectorRef,
     private historiaClinicaService: HistoriaClinicaService,
     private odontogramaService: OdontogramaService,
     private fb: FormBuilder
   ) {
     this.tipoOdontograma = 'geometrico';
     this.isLoading = true;
-    this.cdr.markForCheck();
     this.odontograma = [];
-    this.form = this.fb.group({
-      especificaciones: ['', Validators.required],
-      observaciones: ['', Validators.required],
-    });
-  }
-
-  closeModal(): void {
-    this.modalOpen = false;
-    this.selectedTooth = null;
-    this.selectedRowIndex = null;
-    this.selectedTreatment = '';
-  }
-
-  openModal(toothId: string): void {
-    this.modalOpen = true;
-    this.selectedTooth = toothId;
-
-    const existingToothIndex = this.datos.findIndex(
-      (tooth) => tooth.pieza === toothId
-    );
-
-    if (existingToothIndex !== -1) {
-      this.selectedRowIndex = existingToothIndex;
-      this.selectedTreatment = this.datos[existingToothIndex].tratamiento;
-    } else {
-      this.selectedRowIndex = null;
-      this.selectedTreatment = '';
-    }
-  }
-
-  addTreatment(): void {
-    if (!this.selectedTooth || !this.selectedTreatment) {
-      return;
-    }
-
-    const selectedFace = (
-      document.getElementById('selcara') as HTMLSelectElement
-    ).value;
-    const color = this.tratamientoColores[this.selectedTreatment];
-    const iconPath = `assets/tratamientos/icono-${this.selectedTreatment.toLowerCase()}.png`;
-
-    const newTreatment: ToothTreatment = {
-      tratamiento: this.selectedTreatment,
-      pieza: this.selectedTooth,
-      cara: selectedFace,
-      color: color,
-      icono: iconPath,
-    };
-
-    if (this.selectedRowIndex !== null) {
-      this.datos[this.selectedRowIndex] = newTreatment;
-    } else {
-      this.datos.push(newTreatment);
-    }
-
-    this.closeModal();
+    this.form = this.fb.group({});
+    this.initializeForm();
   }
 
   ngOnInit(): void {
@@ -212,14 +65,7 @@ export class OdontogramaComponent {
       this.userAuth = userAuth;
       this.isLoading = false;
     });
-    this.form = this.fb.group({
-      especificaciones: ['', Validators.required],
-      observaciones: ['', Validators.required],
-      odontograma: [
-        Object.keys(this.odontograma).length > 0,
-        Validators.requiredTrue,
-      ],
-    });
+    this.initializeForm();
 
     this.odontogramaService.currentOdontograma.subscribe((odontograma) => {
       this.odontograma = odontograma;
@@ -240,6 +86,17 @@ export class OdontogramaComponent {
     });
   }
 
+  initializeForm() {
+    this.form = this.fb.group({
+      especificaciones: ['', Validators.required],
+      observaciones: ['', Validators.required],
+      odontograma: [
+        Object.keys(this.odontograma).length > 0,
+        Validators.requiredTrue,
+      ],
+    });
+  }
+
   isFormValid() {
     if (this.form.valid) {
       return true;
@@ -250,6 +107,7 @@ export class OdontogramaComponent {
   }
 
   openConfirmationModal() {
+    this.form.markAllAsTouched();
     if (this.isFormValid()) {
       const numDientes = Object.keys(this.odontograma).length;
       const dientesTexto = numDientes > 1 ? 'dientes' : 'diente';
@@ -266,8 +124,6 @@ export class OdontogramaComponent {
             this.onSave();
           }
         });
-
-      this.modal.onConfirm.subscribe(() => this.onSave());
     }
   }
 
@@ -327,17 +183,5 @@ export class OdontogramaComponent {
           console.error('Error al guardar odontograma:', error);
         });
     }
-  }
-
-  getToothIcon(toothId: string): string | undefined {
-    return this.datos.find((tooth) => tooth.pieza === toothId)?.icono;
-  }
-
-  getToothColor(toothId: string): string | undefined {
-    return this.datos.find((tooth) => tooth.pieza === toothId)?.color;
-  }
-
-  eliminarFila(index: number): void {
-    this.datos.splice(index, 1);
   }
 }
