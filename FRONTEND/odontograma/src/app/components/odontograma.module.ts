7c33640b459e92fed7792682f35df3c600c8ecce
WalterPimentel <70056787@continental.edu.pe>
2024-06-13 11:06:24 -0500

    Logrado: Script de arrange general automatizado

--- FRONTEND/odontograma/src/app/components/odontograma.module.ts
+++ FRONTEND/odontograma/src/app/components/odontograma.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TooltipModule  } from '@amin-karimi/ng2-tooltip-directive';
 
 import { OdontogramaComponent } from './odontograma/odontograma.component';
